c4355c94f9ca09b6bc7aefcc6a558543b5252b6a
iarroyo <iarroyoescobar@gmail.com>
2026-01-21 09:24:01 +0100

    configure gh actions + dependabot

--- vite.config.mjs
+++ vite.config.mjs
@@ -8,6 +8,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [
     classicEmberSupport(),
     ember(),
